ab4f31ec822634076d2efa1b656ec93128fc9917
Christian <christian@styla.com>
2015-07-13 17:56:32 +0200

    revealjs working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,17 @@
 // jshint devel:true
 console.log('\'Allo \'Allo!');
+
+// Init Reveal.js
+Reveal.initialize({
+    history: true,
+    transition: 'slide',
+    center: 'true',
+    controls: false,
+    progress: false
+});
+
+// Slide changed
+Reveal.addEventListener( 'slidechanged', function( event ) {
+    var state = Reveal.getCurrentSlide();
+    console.log(state);
+} );
