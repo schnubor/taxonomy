5d36751c0b5aa3ee54edbdf143699b2fd4336cba
schnubor <info@chko.org>
2015-07-22 23:07:08 +0200

    updated colors and gradients

--- app/scripts/gradient.js
+++ app/scripts/gradient.js
@@ -11,6 +11,8 @@ var updateGradient = function(targetGradient){
     var gradient_percent1 = currentGradient[0];
     var gradient_percent2 = currentGradient[1];
     var gradient_percent3 = currentGradient[2];
+    var gradient_percent4 = currentGradient[3];
+    var gradient_percent5 = currentGradient[4];
 
     clearInterval(animation);
 
@@ -43,10 +45,27 @@ var updateGradient = function(targetGradient){
         else if(gradient_percent3 < targetGradient[2]){
             gradient_percent3++;
         }
+
+        // Percentage 4
+        if(gradient_percent4 > targetGradient[3]){
+            gradient_percent4--;
+        }
+        else if(gradient_percent4 < targetGradient[3]){
+            gradient_percent4++;
+        }
+
+        // Percentage 5
+        if(gradient_percent5 > targetGradient[4]){
+            gradient_percent5--;
+        }
+        else if(gradient_percent5 < targetGradient[4]){
+            gradient_percent5++;
+        }
+
         // apply new values
-        $('body').css('background', 'linear-gradient(to right, #c73d3d 0%,#75cadc '+gradient_percent1+'%,#0a64d0 '+gradient_percent2+'%,#63ae5b '+gradient_percent3+'%,#f2e11a 100%)');
+        $('body').css('background', 'linear-gradient(to right, #e53c4e '+gradient_percent1+'%,#f7925c '+gradient_percent2+'%,#ba7cd4 '+gradient_percent3+'%,#1fc756 '+gradient_percent4+'%,#fee533 '+gradient_percent5+'%)');
         // update current gradient
-        currentGradient = [gradient_percent1, gradient_percent2, gradient_percent3];
+        currentGradient = [gradient_percent1, gradient_percent2, gradient_percent3, gradient_percent4, gradient_percent5];
         // next cycle
         interval++;
     }, 10);
