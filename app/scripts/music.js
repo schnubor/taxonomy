0988e3e9dd6adf61f4d2054db461c3959bbb14e6
schnubor <info@chko.org>
2015-07-23 23:55:41 +0200

    2015 player ready

--- app/scripts/music.js
+++ app/scripts/music.js
@@ -1,4 +1,5 @@
 var player = $('#music');
+var player2 = $('#music2');
 
 $('.js-play').click(function(){
     var _self = $(this);
@@ -29,6 +30,35 @@ $('.js-play').click(function(){
     }
 
     player[0].onended = function(){
+        _self.find('img').attr('src', '/images/icons/play_audio.svg');
+    }
+});
+
+$('.js-play2').click(function(){
+    var _self = $(this);
+    var file = '/files/'+$(this).data('file')+'.mp3';
+    // stop other players
+    miniplayer[0].pause();
+    player[0].pause();
+
+    if(player2[0].paused){
+        player2[0].play();
+        _self.find('img').attr('src', '/images/icons/pause_audio.svg');
+    }
+    else{
+        player2[0].pause();
+        _self.find('img').attr('src', '/images/icons/play_audio.svg');
+    }
+
+    player2[0].ontimeupdate = function(){
+        var duration = player2[0].duration;
+        var currentTime = player2[0].currentTime;
+        var progress = currentTime / (duration/100);
+        var rotation = progress * 3.6;
+        _self.find('.rotator').css({ WebkitTransform: 'rotate(' + rotation + 'deg)'});
+    }
+
+    player2[0].onended = function(){
         _self.find('img').attr('src', '/images/icons/play_audio.svg');
     }
 });
